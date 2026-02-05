5b5776aa584982c88635422020823aea261253c0
Katerina Patticha <aikaterini.patticha@elastic.co>
2026-02-05 11:44:58 +0200

    Refactor histogram fields to include `tdigest` and `legacy` formats in the data model.

--- src/simulators/histograms-metrics-generator.ts
+++ src/simulators/histograms-metrics-generator.ts
@@ -1,24 +1,32 @@
-import { MetricsGenerator } from '../types/simulator-types';
-import { HistogramsConfig, HistogramsMetrics } from '../types/histogram-types';
-import { HistogramsGenerator } from './histograms-generator';
+import { MetricsGenerator } from "../types/simulator-types";
+import { HistogramsConfig, HistogramsMetrics } from "../types/histogram-types";
+import { HistogramsGenerator } from "./histograms-generator";
 
-export class HistogramsMetricsGenerator implements MetricsGenerator<HistogramsConfig, HistogramsMetrics> {
+export class HistogramsMetricsGenerator implements MetricsGenerator<
+  HistogramsConfig,
+  HistogramsMetrics
+> {
   private generator: HistogramsGenerator;
 
   constructor() {
     this.generator = new HistogramsGenerator();
   }
 
-  generateMetrics(config: HistogramsConfig, timestamp: Date): HistogramsMetrics {
+  generateMetrics(
+    config: HistogramsConfig,
+    timestamp: Date,
+  ): HistogramsMetrics {
     return {
       timestamp,
       entity: config,
       histograms: {
         tdigest: this.generator.generateTdigestHistogram(config, timestamp),
-        hdr: this.generator.generateHdrHistogram(config, timestamp),
-        exponential: this.generator.generateExponentialHistogram(config, timestamp)
-      }
+        legacy: this.generator.generateLegacyHistogram(config, timestamp),
+        exponential: this.generator.generateExponentialHistogram(
+          config,
+          timestamp,
+        ),
+      },
     };
   }
 }
-
